d0673e1c9b0864febb15ae516b9c5ea58a2b1de5
achim-t <achim.t@gmail.com>
2023-06-21 11:01:55 +0200

    raw mode and ctrl-q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use std::io::{self, Read};
 
 use crossterm::{
-    terminal
+    terminal,
 };
 fn main() {
     terminal::enable_raw_mode().unwrap();
+
+    fn to_ctrl_byte(c: char) -> u8 {
+        let byte = c as u8;
+        byte & 0b0001_1111
+    }
+
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b,c);
+        }
+        if b == to_ctrl_byte('q') {
             break;
         }
     }
