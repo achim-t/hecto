b244752f62584692284b549f3e66659d8b7f85c0
achim-t <achim.t@gmail.com>
2023-06-21 13:13:14 +0200

    refactoring

---
+++ src/editor.rs
@@ -0,0 +1,36 @@
+use crossterm::{
+    event::{read, Event, KeyCode, KeyModifiers},
+    terminal,
+};
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        terminal::enable_raw_mode().unwrap();
+
+        loop {
+            let event = read();
+            match event {
+                Ok(event) => {
+                    if let Event::Key(pressed_key) = event {
+                        match (pressed_key.modifiers, pressed_key.code) {
+                            (KeyModifiers::CONTROL, KeyCode::Char('q')) => break,
+                            (_, KeyCode::Char(c)) => {
+                                if c.is_control() {
+                                    println!("{:?}\r", c as u8);
+                                } else {
+                                    println!("{:?} ({})\r", c as u8, c)
+                                }
+                            }
+                            _ => println!("{:?}\r", pressed_key),
+                        }
+                    }
+                }
+                Err(err) => die(err),
+            }
+        }
+    }
+}
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,7 @@
-use crossterm::{
-    event::{read, Event, KeyCode, KeyModifiers},
-    terminal,
-};
+mod editor;
+use editor::Editor;
+
 fn main() {
-    terminal::enable_raw_mode().unwrap();
-
-    fn die(e: std::io::Error) {
-        panic!("{}", e);
-    }
-
-    loop {
-        let event = read();
-        match event {
-            Ok(event) => {
-                if let Event::Key(pressed_key) = event {
-                    match (pressed_key.modifiers, pressed_key.code) {
-                        (KeyModifiers::CONTROL, KeyCode::Char('q')) => break,
-                        (_, KeyCode::Char(c)) => {
-                            if c.is_control() {
-                                println!("{:?}\r", c as u8);
-                            } else {
-                                println!("{:?} ({})\r", c as u8, c)
-                            }
-                            
-                        }
-                        _ => println!("{:?}\r", pressed_key),
-                    }
-                }
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor {};
+    editor.run();
 }
