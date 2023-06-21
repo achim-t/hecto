1324c8d19135594145b08e963672a0de8f637d3a
achim-t <achim.t@gmail.com>
2023-06-21 12:46:50 +0200

    use keys instead of bytes; idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
-use std::io::{self, Read};
-
 use crossterm::{
+    event::{read, Event, KeyCode, KeyModifiers},
     terminal,
 };
 fn main() {
     terminal::enable_raw_mode().unwrap();
 
-    fn to_ctrl_byte(c: char) -> u8 {
-        let byte = c as u8;
-        byte & 0b0001_1111
+    fn die(e: std::io::Error) {
+        panic!("{}", e);
     }
 
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b,c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
+    loop {
+        let event = read();
+        match event {
+            Ok(event) => {
+                if let Event::Key(pressed_key) = event {
+                    match (pressed_key.modifiers, pressed_key.code) {
+                        (KeyModifiers::CONTROL, KeyCode::Char('q')) => break,
+                        (_, KeyCode::Char(c)) => {
+                            if c.is_control() {
+                                println!("{:?}\r", c as u8);
+                            } else {
+                                println!("{:?} ({})\r", c as u8, c)
+                            }
+                            
+                        }
+                        _ => println!("{:?}\r", pressed_key),
+                    }
+                }
+            },
+            Err(err) => die(err),
         }
     }
 }
