59f155e79b7b5927c671733109db6b8bc0543adc
achim-t <achim.t@gmail.com>
2023-06-28 13:49:41 +0200

    forward and backward search

--- src/editor.rs
+++ src/editor.rs
@@ -13,6 +13,12 @@ const STATUS_FG_COLOR: Color = Color::Rgb { r: 63, g: 63, b: 63 };
 const STATUS_BG_COLOR: Color = Color::Rgb { r: 239, g: 239, b: 239 };
 const QUIT_TIMES: u8 = 3;
 
+#[derive(PartialEq, Clone, Copy)]
+pub enum SearchDirection {
+    Forward,
+    Backward,
+}
+
 #[derive(Default, Clone)]
 pub struct Position {
     pub x: usize,
@@ -330,8 +336,8 @@ impl Editor {
             print!("{}", text);
         }
     }
-    fn prompt<C>(&mut self, prompt: &str, callback: C) -> Result<Option<String>, std::io::Error>
-        where C: Fn(&mut Self, KeyEvent, &String)
+    fn prompt<C>(&mut self, prompt: &str, mut callback: C) -> Result<Option<String>, std::io::Error>
+        where C: FnMut(&mut Self, KeyEvent, &String)
     {
         let mut result = String::new();
         loop {
@@ -372,22 +378,38 @@ impl Editor {
 
     fn search(&mut self) {
         let old_position = self.cursor_position.clone();
-        if
-            let Some(query) = self
-                .prompt("Search: ", |editor, _, query| {
-                    if let Some(position) = editor.document.find(&query) {
-                        editor.cursor_position = position;
-                        editor.scroll();
+        let mut direction = SearchDirection::Forward;
+        let query = self
+            .prompt("Search (ESC to cancel, Arrows to navigate): ", |editor, key, query| {
+                let mut moved = false;
+                match key.code {
+                    KeyCode::Right | KeyCode::Down => {
+                        direction = SearchDirection::Forward;
+                        editor.move_cursor(KeyCode::Right);
+                        moved = true;
                     }
-                })
-                .unwrap_or(None)
-        {
-            if let Some(position) = self.document.find(&query[..]) {
-                self.cursor_position = position;
-            } else {
-                self.status_message = StatusMessage::from(format!("Not found :{}", query));
-            }
-        } else {
+                    KeyCode::Left | KeyCode::Up => {
+                        direction = SearchDirection::Backward;
+                    }
+                    _ => {
+                        direction = SearchDirection::Forward;
+                    }
+                }
+                if
+                    let Some(position) = editor.document.find(
+                        &query,
+                        &editor.cursor_position,
+                        direction
+                    )
+                {
+                    editor.cursor_position = position;
+                    editor.scroll();
+                } else if moved {
+                    editor.move_cursor(KeyCode::Left)
+                }
+            })
+            .unwrap_or(None);
+        if query.is_none() {
             self.cursor_position = old_position;
             self.scroll();
         }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod editor;
 mod row;
 mod terminal;
 pub use document::Document;
+pub use editor::SearchDirection;
 pub use row::Row;
 use editor::Editor;
 pub use terminal::Terminal;
